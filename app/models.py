7ec772dc17a401e4db23a6ba39f6775c38e6f674
Ivashka <125472587+IvashkaKid@users.noreply.github.com>
2024-12-11 11:08:17 +0300

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,129 @@
+﻿from email.policy import default
+from django.db import models 
+from django.contrib import admin 
+from datetime import datetime
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, unique_for_date="posted", verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Краткое содержание")
+    content = models.TextField(verbose_name="Полное содержание")
+    posted = models.DateTimeField(default=datetime.now(), db_index=True, verbose_name="Опубликована")
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, verbose_name="Автор")
+    image = models.FileField(default = 'temp.jmp', verbose_name = "Путь к картинке")
+
+    def get_absolute_url(self): 
+        return reverse("blogpost", args=[str(self.id)])
+    
+    def __str__(self): 
+        return self.title
+
+    class Meta:
+        db_table = "Posts"
+        ordering = ["-posted"]
+        verbose_name = "статья блога"
+        verbose_name_plural = "статьи блога"
+
+admin.site.register(Blog)
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name="Текст комментария")
+    date = models.DateTimeField(default=datetime.now(), db_index=True, verbose_name="Дата комментария")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор комментария")
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE, verbose_name="Статья комментария")
+
+    def __str__(self):
+        return 'Комментрарий %d %s к %s' % (self.id, self.author, self.post)
+
+    class Meta:
+        db_table = "Comment"
+        ordering = ["-date"]
+        verbose_name = "Комментарии к статье блога"
+        verbose_name_plural = "Комментарии к статьям блога"
+
+admin.site.register(Comment)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название категории")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+admin.site.register(Category)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название товара")
+    description = models.TextField(verbose_name="Описание товара")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    image = models.FileField(default = 'temp.jmp', verbose_name = "Путь к картинке")
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+admin.site.register(Product)
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Товар")
+    quantity = models.PositiveIntegerField(default=1, verbose_name="Количество")
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+
+    class Meta:
+        verbose_name = "Товар в корзине"
+        verbose_name_plural = "Товары в корзине"
+
+admin.site.register(CartItem)
+
+class Status(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название статуса")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Статус заказа"
+        verbose_name_plural = "Статусы заказов"
+
+admin.site.register(Status)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    order_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата заказа")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Общая стоимость")
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, verbose_name="Статус заказа")
+
+    def __str__(self):
+        return f"Заказ №{self.id} от {self.order_date}"
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+
+admin.site.register(Order)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name="Заказ")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Товар")
+    quantity = models.PositiveIntegerField(verbose_name="Количество")
+    price_at_time_order = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена на момент заказа")
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} в заказе №{self.order.id}"
+
+    class Meta:
+        verbose_name = "Товар в заказе"
+        verbose_name_plural = "Товары в заказе"
+
+admin.site.register(OrderItem)
---
+++ app/views.py
@@ -0,0 +1,394 @@
+﻿"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpRequest
+from django.contrib.auth.forms import UserCreationForm
+from .forms import AnketaForm
+
+from django.db import models
+from .models import Blog
+
+from .models import Comment 
+from .forms import CommentForm
+
+from .forms import BlogForm
+
+from .models import Category
+
+from .models import Product
+
+from .models import CartItem
+
+from .models import Status
+
+from .models import Order
+
+from .models import OrderItem
+
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Домашняя страница',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Контакты',
+            'message':'Страница с нашими контактами.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'О нас',
+            'message':'Сведения о нас.',
+            'year':datetime.now().year,
+        }
+    )
+
+def links(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/links.html',
+        {
+            'title':'Полезные ресурсы',
+            'message':'Полезные ресурсы',
+            'year':datetime.now().year,
+        }
+    )
+
+def pool(request):
+    assert isinstance(request, HttpRequest)
+    data = None
+    gender = {'1': 'Мужчина', '2': 'Женщина'}
+    visit = {'1': 'В течение месяца', '2': 'Больше месяца назад', '3': 'Ещё ничего не покупал'}
+
+    if request.method == 'POST':
+        form = AnketaForm(request.POST)
+        if form.is_valid():
+            data = dict()
+            data['name'] = form.cleaned_data['name']
+            data['phone'] = form.cleaned_data['phone']
+            data['gender'] = gender[form.cleaned_data['gender']]
+            data['visit'] = visit[form.cleaned_data['visit']]
+            data['notice'] = 'Да' if form.cleaned_data['notice'] else 'Нет'
+            data['message'] = form.cleaned_data['message']
+            form = None
+    else:
+        form = AnketaForm()
+
+    return render(
+        request,
+        'app/pool.html',
+        {
+            'form': form, 
+            'data': data
+        }
+    )
+
+def registration(request):
+    """Renders the registration page."""
+    assert isinstance(request, HttpRequest)
+
+    if request.method == "POST":  # после отправки формы
+        regform = UserCreationForm(request.POST)
+        
+        if regform.is_valid():  # валидация полей формы
+            reg_f = regform.save(commit=False)  # не сохраняем автоматически данные формы
+            reg_f.is_staff = False  # запрещен вход в административный раздел
+            reg_f.is_active = True  # активный пользователь
+            reg_f.is_superuser = False  # не является суперпользователем
+            reg_f.date_joined = datetime.now()  # дата регистрации
+            reg_f.last_login = datetime.now()  # дата последней авторизации
+            reg_f.save()  # сохраняем изменения после добавления данных
+            return redirect('home')  # переадресация на главную страницу после регистрации
+
+    else:
+        regform = UserCreationForm()  # создание объекта формы для ввода данных нового пользователя
+    
+    return render(
+        request,
+        'app/registration.html',
+        {
+            'regform': regform,  # передача формы в шаблон веб-страницы
+            'year': datetime.now().year,
+        }
+    )
+
+def blog(request):
+    posts = Blog.objects.all()
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/blog.html',
+        {
+            'title': 'Новости',
+            'posts': posts,
+            'year': datetime.now().year,
+        }
+    )
+def blogpost(request, parametr):
+    post_1 = Blog.objects.get(id=parametr)
+    comments = Comment.objects.filter(post=parametr)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment_f = form.save(commit=False)
+            comment_f.author = request.user
+            comment_f.date = datetime.now()
+            comment_f.post = Blog.objects.get(id=parametr)
+            comment_f.save()
+            return redirect('blogpost', parametr=post_1.id)
+    else:
+        form = CommentForm()
+    return render(
+        request,
+        'app/blogpost.html',
+        {
+            'post_1': post_1,
+            'comments': comments,
+            'form': form,
+            'year': datetime.now().year,
+        }
+    )
+
+def newpost(request):
+    """Renders the newpost page."""
+    assert isinstance(request, HttpRequest)
+    if request.method == "POST":
+        blogform = BlogForm(request.POST, request.FILES)
+        if blogform.is_valid():
+            blog_f = blogform.save(commit=False)
+            blog_f.posted = datetime.now()
+            blog_f.autor = request.user
+            blog_f.save()
+            return redirect('blog')
+    else:
+        blogform = BlogForm()
+    return render(
+        request,
+        'app/newpost.html',
+        {
+            'blogform': blogform,
+            'title': 'Добавить статью блога',
+            'year': datetime.now().year,
+        }
+    )
+
+def videopost(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/videopost.html',
+        {
+            'title':'Видео',
+            'message':'Видео',
+            'year':datetime.now().year,
+        }
+    )
+
+def catalog(request):
+    categories = Category.objects.all()
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/catalog.html',
+        {
+            'categories': categories,
+            'year': datetime.now().year,
+        }
+    )
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(
+        request,
+        'app/product_detail.html',
+        {
+            'product': product,
+            'year': datetime.now().year,
+        }
+    )
+
+def cart(request):
+    if request.user.is_authenticated:
+        cart_items = CartItem.objects.filter(user=request.user)
+    else:
+        cart_items = []
+
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+
+    if request.method == 'POST':
+        if not cart_items:
+            return redirect('cart')
+
+        status = Status.objects.get(name='Новый')  # Предполагается, что у вас есть статус "Новый"
+        order = Order.objects.create(user=request.user, total_price=total_price, status=status)
+
+        # Создание позиций заказа
+        for item in cart_items:
+            OrderItem.objects.create(
+                order=order,
+                product=item.product,
+                quantity=item.quantity,
+                price_at_time_order=item.product.price
+            )
+
+        # Очистка корзины
+        cart_items.delete()
+
+        return redirect('order_detail', order_id=order.id)
+
+    return render(
+        request,
+        'app/cart.html',
+        {
+            'cart_items': cart_items,
+            'total_price': total_price,
+            'year': datetime.now().year,
+        }
+    )
+
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    quantity = int(request.POST.get('quantity', 1))
+
+    if request.user.is_authenticated:
+        cart_item, created = CartItem.objects.get_or_create(user=request.user, product=product)
+        if created:
+            cart_item.quantity = quantity
+        else:
+            cart_item.quantity += quantity
+        cart_item.save()
+    else:
+        # Handle anonymous user cart using session
+        cart = request.session.get('cart', {})
+        cart_item_id = str(product.id)
+        if cart_item_id in cart:
+            cart[cart_item_id]['quantity'] += quantity
+        else:
+            cart[cart_item_id] = {'quantity': quantity}
+        request.session['cart'] = cart
+
+    # Обновляем информацию о корзине
+    if request.user.is_authenticated:
+        cart_items = CartItem.objects.filter(user=request.user)
+    else:
+        cart_items = [
+            {'product': Product.objects.get(id=int(item_id)), 'quantity': details['quantity']}
+            for item_id, details in request.session.get('cart', {}).items()
+        ]
+
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+
+    # Возвращаем HTML-ответ с уведомлением
+    return render(
+        request,
+        'app/add_to_cart_notification.html',
+        {
+            'product_name': product.name,
+            'cart_items': cart_items,
+            'total_price': total_price,
+            'year': datetime.now().year,
+        }
+    )
+
+def remove_from_cart(request, cart_item_id):
+    cart_item = get_object_or_404(CartItem, id=cart_item_id)
+    if request.user.is_authenticated and cart_item.user == request.user:
+        cart_item.delete()
+    return redirect('cart')
+
+def update_cart(request, cart_item_id):
+    cart_item = get_object_or_404(CartItem, id=cart_item_id)
+    if request.user.is_authenticated and cart_item.user == request.user:
+        quantity = int(request.POST.get('quantity', 1))
+        if quantity > 0:
+            cart_item.quantity = quantity
+            cart_item.save()
+        else:
+            cart_item.delete()
+    return redirect('cart')
+
+def order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    order_items = OrderItem.objects.filter(order=order)
+    return render(request, 'app/order_detail.html', {'order': order, 'order_items': order_items})
+
+
+def order_list(request):
+    if request.user.is_authenticated:
+        orders = Order.objects.filter(user=request.user).order_by('-order_date')
+    else:
+        orders = []
+
+    return render(
+        request,
+        'app/order_list.html',
+        {
+            'orders': orders,
+            'year': datetime.now().year,
+        }
+    )
+
+def manage_orders(request):
+    orders = Order.objects.all().order_by('-order_date')
+    return render(
+        request,
+        'app/manage_orders.html',
+        {
+            'orders': orders,
+            'year': datetime.now().year,
+        }
+    )
+
+def manage_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    order_items = OrderItem.objects.filter(order=order)
+    statuses = Status.objects.all()
+
+    if request.method == 'POST':
+        new_status_id = request.POST.get('status')
+        new_status = get_object_or_404(Status, id=new_status_id)
+        order.status = new_status
+        order.save()
+        return redirect('manage_order_detail', order_id=order.id)
+
+    return render(
+        request,
+        'app/manage_order_detail.html',
+        {
+            'order': order,
+            'order_items': order_items,
+            'statuses': statuses,
+            'year': datetime.now().year,
+        }
+    )
+
+def home(request):
+    latest_blogs = Blog.objects.order_by('-posted')[:3]
+    return render(request, 'app/index.html', {'latest_blogs': latest_blogs})
